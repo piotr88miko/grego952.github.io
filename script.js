8b7630f2ab529ad24623f3c2b834676883c8bc0c
Grzegorz <karaluch26@wp.pl>
2021-03-16 21:58:39 +0100

    corrected script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://guarded-depths-21843.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const trelloApiRoot = 'https://guarded-depths-21843.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
